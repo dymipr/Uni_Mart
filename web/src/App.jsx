c165465d36d933e77afc0e9c76b562967875ad77
ProAardvark <proaardvark0608@proton.me>
2024-03-28 09:35:39 -0700

    insert Url Router

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/pages/home';
+
+export const CarContext = createContext(null);
+
 function App() {
 
   return (
-    <>
-      <Home />
-    </>
+    <CarContext.Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CarContext.Provider>
   );
 }
 
