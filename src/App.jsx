5cc6eafc17e5b01d52c019dbea652f012909b46f
ProAardvark <proaardvark0608@proton.me>
2024-03-27 18:31:11 -0700

    Create Repository & create hero page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import "./App.css";
+
+import Hero from "./components/Views/Hero";
+
+function App() {
+
+  return (
+    <div id='home'>
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
