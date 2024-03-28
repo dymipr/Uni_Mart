371a1b8859a74220670ba01f625b9fe445a02214
ProAardvark <proaardvark0608@proton.me>
2024-03-28 09:18:53 -0700

    fixed config directories and files

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,32 +1,12 @@
 import React, { useState } from 'react';
 import "./App.css";
-
-import Navbar from "./components/Common/Navbar";
-import Hero from "./components/Views/Hero";
-import NyxCipher from "./components/Views/nyxCipher";
-import Advantage from "./components/Views/advantage";
-import NyxToolkit from "./components/Views/nyxToolkit";
-import NyxVsPaal from "./components/Views/nyxVsPaal";
-import Partnering from "./components/Views/partnering";
-import JoinUs from "./components/Views/joinUs";
-import Faqs from "./components/Views/faqs";
-import Footer from "./components/Views/footer";
-
+import Home from './components/pages/home';
 function App() {
 
   return (
-    <div id='home'>
-      <Navbar />
-      <Hero />
-      <NyxCipher />
-      <Advantage />
-      <NyxToolkit />
-      <NyxVsPaal />
-      <Partnering />
-      <JoinUs />
-      <Faqs />
-      <Footer />
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
