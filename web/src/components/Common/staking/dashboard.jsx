7b2ed0e7ffc2158aa4c94125dd711ca4aa14b412
ProAardvark <proaardvark0608@proton.me>
2024-03-28 20:56:39 -0700

    Update dashboard.jsx

--- web/src/components/Common/staking/dashboard.jsx
+++ web/src/components/Common/staking/dashboard.jsx
@@ -1,14 +1,16 @@
 import Stake from "./stake";
+import Historyview from "./historyView";
+import Connect from "../buttons/connectwallet";
 
 function Dashboard() {
   return (
-    <div className="font-[RobotoMono] w-full lg:p-8 p-4 gap-10">
+    <div className="font-[RobotoMono] w-full lg:p-8 p-4 flex flex-col gap-10">
       <div className="md:flex hidden items-center justify-between">
-        <h1 className="text-[23px] font-extrabold bg-gradient-to-r from-[#5AB0FF] to-[#01FFC2] text-transparent bg-clip-text mt-2 mb-10">
+        <h1 className="text-[23px] font-extrabold bg-gradient-to-r from-[#5AB0FF] to-[#01FFC2] text-transparent bg-clip-text mt-2">
           Staking
         </h1>
         <div className="flex items-center">
-          {/* <Connect /> */}Connect
+          <Connect />
         </div>
       </div>
       <div className="flex flex-col w-full items-start gap-5">
@@ -22,16 +24,33 @@ function Dashboard() {
           </p>
         </div>
         <div className="grid lg:grid-cols-3 grid-cols-1 md:gap-10 gap-5 w-full ">
-            <Stake
-              data={{
-                'days': 14,
-                'nyxpercent': "1.20",
-                'percent': 3,
-                'progressbar': 3
-              }} />
+          <Stake
+            data={{
+              days: 14,
+              nyxpercent: "1.20",
+              percent: 3,
+              progressbar: 3,
+            }}
+          />
+          <Stake
+            data={{
+              days: 28,
+              nyxpercent: "6",
+              percent: 10,
+              progressbar: 5,
+            }}
+          />
+          <Stake
+            data={{
+              days: 56,
+              nyxpercent: "92.8",
+              percent: 15,
+              progressbar: 10,
+            }}
+          />
         </div>
       </div>
-      {/* <Historyview /> */}Historyview
+      <Historyview />
     </div>
   );
 }
