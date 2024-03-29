1cdcb0c22720394c79c11ae7b9d84c537029ef42
ProAardvark <proaardvark0608@proton.me>
2024-03-29 06:06:55 -0700

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/pages/home';
 import Staking from './components/pages/staking';
+import ModalNavbar from './components/Common/staking/modalNavbar';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/">
             <Route index element={<Home />} />
             <Route path="staking" element={<Staking />} />
+            <Route path="modal" element={<ModalNavbar />} />
           </Route>
         </Routes>
       </BrowserRouter>
